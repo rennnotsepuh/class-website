2e6e267f1378fa5d689a5c9c3964d26769f4c637
xio <144570365+rennnotsepuh@users.noreply.github.com>
2024-08-23 07:43:08 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,38 @@
+const data = {
+
+
+
+
+  // ISI NAMA DI DALAM KUTIP
+  // TANDA KUTIPANNYA JANGAN DIHAPUS YA!!
+
+  namaguru: "",
+  namabendahara: "",
+  namasekretaris: "",
+  nama1: "",
+  nama2: "",
+  nama3: "",
+  nama4: "",
+  nama5: "",
+  nama6: "",
+  nama7: "",
+  nama8: "",
+  nama9: "",
+  nama10: "",
+  nama11: "",
+  nama12: "",
+  nama13: "",
+  nama14: "",
+  nama15: "",
+  nama16: "",
+  nama4: "",
+  nama4: "",
+  nama4: "",
+  nama4: "",
+  nama4: "",
+  nama4: "",
+  nama4: "",
+  nama4: "",
+  nama4: "",
+  
+}
